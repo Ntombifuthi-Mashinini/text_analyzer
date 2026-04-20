b9441cb6ff787e56c7a1e650cca9b3fea47aa916
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2026-04-20 23:54:36 +0200

    Implement number to words conversion function

---
+++ num_to_words.py
@@ -0,0 +1,21 @@
+def number_to_words(n):
+    ones = [
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    ]
+
+    tens = [
+        "", "", "twenty", "thirty", "forty",
+        "fifty", "sixty", "seventy", "eighty", "ninety"
+    ]
+
+   
+    if n < 20:
+        return ones[n]
+
+    if n % 10 == 0:
+        return tens[n // 10]
+
+    return tens[n // 10] + "-" + ones[n % 10]
